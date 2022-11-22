cdfb4cdc678ec0543bd6ccc47c368e71baadde81
William Cardoso Barbosa <williancard123@gmail.com>
2022-11-22 08:45:40 -0400

    f

--- 3-P/POO/aula9/calcularJuros/Main.java
+++ 3-P/POO/aula9/calcularJuros/Main.java
@@ -2,8 +2,8 @@ class Main {
     public static void main(String[] args) {
       CalcularJurps cj = new CalcularJurps();
       System.out.println("Juros Simples: " + cj.calcularJurosSimples(1000));
-      System.out.println("Juros Simples: " + cj.calcularJurosSimples(1000, 2));
-      System.out.println("Juros Composto: " + cj.calcularJurosComposto(1000, 2));
+      System.out.println("Juros Simples: " + cj.calcularJurosSimples(1000, 10));
+      System.out.println("Juros Composto: " + cj.calcularJurosComposto(1000, 10));
 
     }
 }
